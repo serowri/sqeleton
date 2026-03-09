0a1affc25e4b1f6e7d13bc6f6d88eb0acbc53e28
serowri <07taitai07+dev@gmail.com>
2026-03-09 01:41:40 +0000

    Add H gate test

---
+++ tests/test_gates.py
@@ -0,0 +1,14 @@
+from sqeleton import *
+import numpy as np
+
+def test_H_gate():
+    state = QuantumState(1)
+    circuit = QuantumCircuit(1)
+    circuit.add_H_gate(0)
+    circuit.update_quantum_state(state)
+
+    expected = np.array([
+        [1/np.sqrt(2)], [1/np.sqrt(2)]
+    ], dtype=complex)
+
+    assert np.allclose(state.state, expected)
