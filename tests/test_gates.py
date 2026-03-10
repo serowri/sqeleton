73e98e1d649d60d458c87a91216aa054b8378944
serowri <07taitai07+dev@gmail.com>
2026-03-10 01:47:07 +0000

    Add X gate test

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -1,6 +1,18 @@
 from sqeleton import *
 import numpy as np
 
+def test_X_gate():
+    state = QuantumState(1)
+    circuit = QuantumCircuit(1)
+    circuit.add_X_gate(0)
+    circuit.update_quantum_state(state)
+
+    expected = np.array([
+        [0], [1]
+    ], dtype=complex)
+
+    assert np.allclose(state.state, expected)
+
 def test_H_gate():
     state = QuantumState(1)
     circuit = QuantumCircuit(1)
