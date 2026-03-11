4af4de37b3c50484ce2ba34792f2f8568111fd95
serowri <07taitai07+dev@gmail.com>
2026-03-11 00:48:48 +0000

    Add 3q GHZ state test

---
+++ tests/test_ghz_state.py
@@ -0,0 +1,16 @@
+from sqeleton import *
+import numpy as np
+
+def test_ghz_3q_1():
+    state = QuantumState(3)
+    circuit = QuantumCircuit(3)
+    circuit.add_H_gate(0)
+    circuit.add_CNOT_gate(0,1)
+    circuit.add_CNOT_gate(0,2)
+    circuit.update_quantum_state(state)
+
+    expected = np.array([
+        [1/np.sqrt(2)], [0], [0], [0], [0], [0], [0], [1/np.sqrt(2)]
+    ], dtype=complex)
+
+    assert np.allclose(state.state, expected)
