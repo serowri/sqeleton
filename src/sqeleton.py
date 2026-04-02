dccc459cca94c0f048e33c105a44db8facdff7db
serowri <07taitai07+dev@gmail.com>
2026-04-02 01:21:36 +0000

    Fix get_depth

--- src/sqeleton.py
+++ src/sqeleton.py
@@ -275,11 +275,7 @@ class QuantumCircuit:
             if key in self._gateBase_2q:
                 backet[value1] = max(backet[value1], backet[value2])+1
                 backet[value2] = backet[value1]
-                left = min(value1, value2)
-                right = max(value1, value2)
-                backet[left:right] = backet[value1]
-                
-            backet[backet < (backet[value1]-1)] = backet[value1]-1
+
         print("circuit depth :", np.max(backet))
         return None
 
